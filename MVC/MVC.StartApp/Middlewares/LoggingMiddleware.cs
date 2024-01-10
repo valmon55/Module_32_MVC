112063c34250c8a77ae1edce20b037618e392d24
valmon55 <fedor_ka@mail.ru>
2024-01-10 18:20:03 +0300

    1

---
+++ MVC/MVC.StartApp/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+
+namespace ASP.StartApp.Middlewares
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        /// <summary>
+        ///  Middleware-компонент должен иметь конструктор, принимающий RequestDelegate
+        /// </summary>
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        /// <summary>
+        ///  Необходимо реализовать метод Invoke  или InvokeAsync
+        /// </summary>
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine($"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}");
+            await _next?.Invoke(context);
+        }
+    }
+}
