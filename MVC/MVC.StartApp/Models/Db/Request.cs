244576e4defe219535b2e83170aa96467cee08ee
valmon55 <fedor_ka@mail.ru>
2024-01-13 20:34:18 +0300

    Task 32.11.1 Start

---
+++ MVC/MVC.StartApp/Models/Db/Request.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System;
+
+namespace MVC.StartApp.Models.Db
+{
+    [Table("Requests")]
+    public class Request
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Url { get; set; }
+    }
+}
