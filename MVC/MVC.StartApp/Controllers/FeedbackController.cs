4fbc06c4d6e1927df65785b02a418d21fc1327dd
valmon55 <fedor_ka@mail.ru>
2024-01-13 17:05:49 +0300

    Пример AJAX запроса

---
+++ MVC/MVC.StartApp/Controllers/FeedbackController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVC.StartApp.Models;
+using System.Diagnostics;
+
+namespace MVC.StartApp.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        /// <summary>
+        ///  Метод, возвращающий страницу с отзывами
+        /// </summary>
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Метод для Ajax-запросов
+        /// </summary>
+        [HttpPost]
+        public IActionResult Add(Feedback feedback)
+        {
+            return StatusCode(200, $"{feedback.From}, спасибо за отзыв!");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC/MVC.StartApp/Models/Feedback.cs
@@ -0,0 +1,8 @@
+﻿namespace MVC.StartApp.Models
+{
+    public class Feedback
+    {
+        public string From { get; set; }
+        public string Text { get; set; }
+    }
+}
