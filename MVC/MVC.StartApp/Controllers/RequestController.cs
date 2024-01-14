7aa1c24bc10a1aa7c725aa76a1a0d9925622496e
valmon55 <fedor_ka@mail.ru>
2024-01-14 15:53:27 +0300

    Вопрос

---
+++ MVC/MVC.StartApp/Controllers/RequestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.StartApp.Models.Db;
+using System;
+using System.Threading.Tasks;
+
+namespace MVC.StartApp.Controllers
+{
+    public class RequestController : Controller
+    {
+        private readonly IRequestRepository _repo;
+        public RequestController(IRequestRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<IActionResult> Index()
+        { 
+            var requests = await _repo.GetURLs();
+            return View(requests);
+        }
+    }
+}
